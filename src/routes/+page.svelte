38e3453565279a6c6c5a9a844a79c70e5f3b609a
yoonminsoo97 <yoonminwater@gmail.com>
2024-10-11 17:15:06 +0900

    view: 메인 페이지 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="position-absolute top-50 start-50 translate-middle">
+  <div class="text-center">
+    <p class="text-primary display-2">Home</p>
+  </div>
+</div>
