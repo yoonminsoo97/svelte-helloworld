e6849041e67784dbd4b053263ce5efc70d2de17f
yoonminsoo97 <yoonminwater@gmail.com>
2024-10-11 17:15:50 +0900

    view: error 페이지 추가

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<div class="position-absolute top-50 start-50 translate-middle">
+  <div class="text-center">
+    <p class="text-danger display-2">404 NOT FOUND</p>
+    <div>
+      <p>페이지를 찾을 수 없습니다.</p>
+    </div>
+    <a href="/" class="text-decoration-none">돌아가기</a>
+  </div>
+</div>
