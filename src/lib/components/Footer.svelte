2a589249c4ac6ee093758972b252af1a209a8d24
yoonminsoo97 <yoonminwater@gmail.com>
2024-10-11 17:14:32 +0900

    view: header, footer 컴포넌트 추가

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,15 @@
+<footer class="footer">
+  <div class="py-3 border-top">
+    <p class="text-center text-body-secondary mb-0">
+      Copyright@2024 Hello, Svelte!!
+    </p>
+  </div>
+</footer>
+
+<style>
+  .footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+  }
+</style>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { page } from '$app/stores';
+
+  const links = [
+    {href: '/', text: 'Home'},
+    {href: '/about', text: 'About'},
+    {href: '/login', text: 'Login'},
+    {href: '/signup', text: 'Signup'},
+  ];
+</script>
+
+<header class="shadow-sm">
+  <div class="container">
+    <nav class="d-flex justify-content-center py-3">
+      {#each links as {href, text}}
+        <a href={href} class:active={$page.url.pathname === href}>
+          {text}
+        </a>
+      {/each}
+    </nav>
+  </div>
+</header>
+
+<style>
+  nav {
+    display: flex;
+    gap: 2rem;
+  }
+
+  nav a {
+    text-decoration: none;
+    color: black;
+  }
+
+  nav a.active {
+    color: #0d6efd;
+  }
+</style>
