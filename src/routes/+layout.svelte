057038bf53ab506cebd83db0c848f3c52a656442
yoonminsoo97 <yoonminwater@gmail.com>
2024-10-11 17:15:24 +0900

    view: 모든 페이지에 적용되는 레이아웃 추가

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import Header from '$lib/components/Header.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<svelte:head>
+  <title>Hello, Svelte!!</title>
+</svelte:head>
+
+<Header />
+
+<div class="container">
+  <slot />
+</div>
+
+<Footer />
